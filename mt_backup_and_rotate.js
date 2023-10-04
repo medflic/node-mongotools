8ce0ff63db4ce7cf8c0d8e4a573ac989adaaf1af
sathish <sathish@alldirectionsource.com>
2023-10-04 10:50:04 +0530

    backup script improved with CRON

--- mt_backup_and_rotate.js
+++ mt_backup_and_rotate.js
@@ -1,11 +1,7 @@
 import os from "os";
 
-// node-mongotools users must use this line
-// import { MongoTools, MTOptions, MTCommand } from "node-mongotools";
-
-// import from inside project
 import { MongoTools, MTOptions, MTCommand } from "./lib/mt.js";
-
+import { CronJob } from 'cron';
 async function dumpAndRotate(uri, path){
   var mt = new MongoTools();
   var mtc = new MTCommand();// to reuse log methods
@@ -25,9 +21,26 @@ async function dumpAndRotate(uri, path){
   mtc.logSuccess(rotationResult);
 }
 
+const backupPath = process.argv.slice(2)[0] ||  `backup/${os.hostname()}`
 
-
-const path = process.argv.slice(2)[0] ||  `backup/${os.hostname()}`
-const uri = process.env.MONGO_URI;
-
-dumpAndRotate(uri, path);
+  const envs = process.env;
+  const sortedEnvs = Object.keys(process.env).sort((a, b) => a - b);
+  const mongoURLS = sortedEnvs.filter(e => e.startsWith('MONGO_URI_'));
+  if (mongoURLS.length === 0) {
+    throw new Error("Nothing to backup");
+  }
+  
+  const backupDirPath = sortedEnvs.filter(e => e.startsWith('BACKUP_DIR_PATH_'));
+  const scheduledTimes = sortedEnvs.filter(e => e.startsWith('SCHEDULED_TIME_'));
+  
+  for (let index = 0; index < mongoURLS.length; index++) {
+    const uri = envs[mongoURLS[index]];
+    const path = envs[backupDirPath[index]] || backupPath
+    const scheduledTime = envs[scheduledTimes[index]] || '0 0 0 * * *';
+    if (process.env.SCHEDULED_BACKUP === 'true') {
+      new CronJob(scheduledTime, () => dumpAndRotate(uri, path), null, true, 'Asia/Kolkata');
+      console.log(`*** SUCCESS BACKUP INITIATED for ${uri.slice(-7)} AND BACKUP TIME IS ${scheduledTime} **** `)
+    } else {
+      dumpAndRotate(uri, path);
+    }
+}
